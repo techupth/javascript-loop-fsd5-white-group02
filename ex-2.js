5c407abf456f911f3fdc497334c2fb876e87e4ec
mortybunyarit <bunyarit.saelim@gmail.com>
2023-12-01 17:22:54 +0700

    feat:loop 6exs done

--- ex-2.js
+++ ex-2.js
@@ -2,5 +2,14 @@
 let numbers = [10, 20, 30, 40, 50];
 let total = 0;
 // Start coding here
+//xercise #2: Sum the Numbers
+// กำหนดให้ numbers เป็น Array ที่ประกอบด้วยตัวเลข
+// ให้ Declare ตัว Variable total โดย Assign ตัว Value 0
+// ให้ใช้ For...of Loop เพื่อคำนวณผลรวมของเลขทุกตัวใน Array
+//เมื่อโปรแกรมทำงานจะแสดงผลลัพธ์ 150
+
+for (let number of numbers) {
+  total += number;
+}
 
 console.log(total);
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,23 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
+let minNumber = numbers[0];
 // Start coding here
+// Exercise #3: Find the Minimum Number
 
+// ให้ numbers เป็น Array ที่ประกอบด้วยตัวเลข
+// ให้ใช้ For Loop แบบดั้งเดิมในการหา Value ที่น้อยที่สุดใน numbers และนำ Value ที่ได้ไปเก็บไว้ใน Variable ที่ชื่อว่า minNumber
+// กำหนดให้โค้ดตั้งต้นมีดังนี้
+
+// Exercise #3: Find a Minimum Number
+// let numbers = [100, 20, 3, 1000];
+// let minNumber;
+
+// console.log(minNumber);
+// เมื่อโปรแกรมทำงานจะแสดงผลลัพธ์ทางหน้าจอแบบนี้
+// 3
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  }
+}
 console.log(minNumber);
